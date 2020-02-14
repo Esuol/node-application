ab7ced5de75a26e6eac6f09e1cb5271b6ec8ed13
berlinen <1292393108@qq.com>
2020-02-14 13:06:36 +0800

    feta: server init

---
+++ server/configs/interceptors.ts
@@ -0,0 +1,9 @@
+import { InterceptorInterface, Action, Interceptor } from "routing-controllers";
+
+@Interceptor()
+export class AutoAssignJSONInterceptor implements InterceptorInterface {
+  intercept(action: Action, content: any): any {
+    if (typeof content === "object") return JSON.stringify(Object.assign({ message: "ok" }, content));
+    return JSON.stringify({ message: content });
+  }
+}
