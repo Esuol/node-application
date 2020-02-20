8382e6a20dda0e6207d9fe1e7ff9645619810546
berlinen <1292393108@qq.com>
2020-02-20 16:17:08 +0800

    feat:🏄🏾‍♀️ 项目初始化

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,19 @@
 
 import * as Koa from "koa";
+const parser  = require("koa-bodyparser");
+const cors = require("koa-cors");
+const views = require("koa-views");
+const koaStatic = require("koa-static");
 
 const app = new Koa();
+const { resolve } = require("path");
+
+app.use(parser());
+app.use(cors());
+app.use(views(resolve(__dirname, "./views"), {
+  extension: "ejs"
+}));
+app.use(koaStatic(__dirname + "./public"));
 
 app.listen(3000, () => {
   console.log("Koa is listening in http://localhost:3000");
