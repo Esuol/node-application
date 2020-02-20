5cfec1ac3a77ad3149ee08ae6f05c2db781dd59a
berlinen <1292393108@qq.com>
2020-02-20 15:32:18 +0800

    fix: init

--- server/app.ts
+++ server/app.ts
@@ -1,17 +1,10 @@
-import { Server } from "http";
-import { print } from "./configs/utils";
-import { Environment } from "./configs/environments";
-import { createServer } from "./configs/application";
-import * as bootstrap from "./configs/bootstrap";
 
-module.exports = (async(): Promise<Server> => {
-  try {
-    const app = await createServer();
-    return app.listen(Environment.port, () => {
-      print.log(`server listening on ${Environment.port}, in ${Environment.identity} mode.`);
-      bootstrap.after();
-    });
-  } catch (e) {
-    console.log(e);
-  }
-})();
+import * as Koa from "koa";
+
+const app = new Koa();
+
+app.listen(3000, () => {
+  console.log("Koa is listening in http://localhost:3000");
+});
+
+module.exports = app;
