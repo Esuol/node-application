9943b66d64c69ababccaa39765c0e4ed660b81e8
berlinen <1292393108@qq.com>
2020-03-02 16:00:20 +0800

    定义管理员模

--- server/app/models/admin.ts
+++ server/app/models/admin.ts
@@ -8,9 +8,57 @@ const {Sequelize, Model} = require("sequelize");
 class Admin extends Model {
 
 }
-class Admin extends Model {
 
-}
+// 初始用户模型
 
+Admin.init({
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+
+  nickname: {
+    type: Sequelize.STRING(64),
+    allowNull: false,
+    comment: "管理员昵称"
+  },
+
+  email: {
+    type: Sequelize.STRING(128),
+    unique: true,
+    allowNull: false,
+    comment: "管理员邮箱"
+  },
+
+  password: {
+    type: Sequelize.STRING,
+    set(val) {
+      // 加密
+      const salt = bcrypt.genSaltSync(10);
+      // 生成加密密码
+      const psw = bcrypt.hashSync(val, salt);
+      this.setDataValue("password", psw);
+    },
+    allowNull: false,
+    comment: "管理员密码"
+  },
+
+  created_at: {
+    type: Sequelize.DATE,
+    allowNull: false,
+    get() {
+      return moment(this.getDataValue("created_at")).format("YYYY-MM-DD");
+    }
+  }
+}, {
+  sequelize,
+  modelName: "admin",
+  tableName: "admin"
+});
+
+module.exports = {
+  Admin
+};
 
 export {};
