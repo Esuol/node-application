4e0599541b89e1a7aaf0b6c3deaa046a866ce8e6
berlinen <1292393108@qq.com>
2020-03-04 13:40:30 +0800

    feat: validator

--- server/app/core/validator.ts
+++ server/app/core/validator.ts
@@ -9,4 +9,52 @@ const {
   ParameterException
 } = require("./http-exception");
 
+const {
+  get,
+  last,
+  set,
+  cloneDeep
+} = require("lodash");
+
+const {
+  findMembers
+} = require("./util");
+
+
+class Validator {
+
+  data: {};
+  parsed: {};
+
+  constructor () {
+    this.data = {};
+    this.parsed = {};
+  }
+
+  _assembleAllParams(ctx) {
+    return {
+      body: ctx.request.body,
+      query: ctx.request.query,
+      path: ctx.params,
+      header: ctx.request.header
+    };
+  }
+
+  get(path, parsed = true) {
+    if (parsed) {
+      // tslint:disable-next-line:no-null-keyword
+      const value = get(this.parsed, path, null);
+      if (value == undefined) {
+        const keys = path.split(".");
+        const key = last(keys);
+        return get(this.parsed.default, key);
+      }
+      return value;
+    } else {
+      return get(this.data, path);
+    }
+  }
+
+}
+
 export {};
