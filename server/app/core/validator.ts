03f2f0c747162fa7512da82f92acb23b5e80085c
berlinen <1292393108@qq.com>
2020-03-05 14:12:42 +0800

    feat: validate

--- server/app/core/validator.ts
+++ server/app/core/validator.ts
@@ -59,6 +59,66 @@ class Validator {
     }
   }
 
+  _findMembersFilter(key) {
+    if (/validate([A-Z])\w+/g.test(key)) {
+      return true;
+    }
+    if (this[key] instanceof Array) {
+      this[key].forEach(value => {
+        const isRuleType = value instanceof Rule;
+        if (!isRuleType) {
+          throw new Error("验证数组必须全部为Rule类型");
+        }
+      });
+      return true;
+    }
+    return false;
+  }
+
+}
+
+
+
+
+class RuleResult {
+  constructor(pass, msg = "") {
+    Object.assign(this, {
+      pass,
+      msg
+    });
+  }
+}
+
+class RuleFieldResult extends RuleResult {
+  legalValue: any;
+  // tslint:disable-next-line:no-null-keyword
+  constructor(pass, msg = "", legalValue = null) {
+    super(pass, msg);
+    this.legalValue = legalValue;
+  }
+}
+class Rule {
+  name: string;
+  params: any;
+  msg: any;
+  message: any;
+
+  constructor(name, msg, ...params) {
+    Object.assign(this, {
+      name,
+      msg,
+      params
+    });
+  }
+
+  validate(field) {
+    if (this.name == "isOptional")
+      return new RuleResult(true);
+    if (!validator[this.name](field + "", ...this.params)) {
+      return new RuleResult(false, this.msg || this.message || "参数错误");
+    }
+    return new RuleResult(true, "");
+  }
 }
 
 export {};
