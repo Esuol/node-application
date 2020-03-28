607b406cc27e43f0de7bfb000606b0a6b0ceb1d9
berlinen <1292393108@qq.com>
2020-03-28 17:00:07 +0800

    feat: extra

--- server/app/core/validator.ts
+++ server/app/core/validator.ts
@@ -267,14 +267,7 @@ _convert(value) {
   return value;
 }
 
-_allowEmpty() {
-  for (const rule of this.rules) {
-    if (rule.name == "isOptional") {
-      return true;
-    }
-  }
-  return false;
-}
+
 
 
 export {};
