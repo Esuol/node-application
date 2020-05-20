b5bd66c58bdb2e2d9a6a0107682e060452887645
“berlinen” <1292393108@qq.com>
2020-05-20 17:48:50 +0800

    feat: 修改request

--- client/app/router/index.js
+++ client/app/router/index.js
@@ -5,10 +5,10 @@
 
 const home = require("../controller/home");
 
-const paths = ['/article.css', '/article.js', '/0.js', '1.js', '2.js', '/' , '/favicon.ico']
-const isCorePath = (path) => {
-  return paths.includes(path)
-}
+// const paths = ["/article.css", "/article.js", "/0.js", "1.js", "2.js", "/", "/favicon.ico"];
+// const isCorePath = (path) => {
+//   return paths.includes(path);
+// };
 
 module.exports = (router) => {
   router.get("/", home.renderHtml);
