f024689b9592e22e3cfc1ceaa2d45fd572f90d05
“berlinen” <1292393108@qq.com>
2020-05-22 14:16:05 +0800

    feat: aliossupload

---
+++ client/web/components/AliOssUpload/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Upload, message } from "antd";
+import moment from "moment";
+import OSS from "ali-oss";
+import { PlusOutlined, LoadingOutlined, InboxOutlined } from "@ant-design/icons";
+import { accessKeySecret, accessKeyId, bucket } from "./secret";
+
+const { Dragger } = Upload;
+
+const client = new OSS({
+  region: "oss-cn-hangzhou",
+  accessKeyId,
+  accessKeySecret,
+  bucket,
+  secure: true,
+});
+
+const UploadToOss = (path, file) => {
+  return new Promise((resolve, reject) => {
+    client
+      .put(path, file)
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+const filePath = (file) => {
+  // 上传文件路径和名称
+  return `${moment().format("YYYYMMDD")}/${file.uid}.${file.type.split("/")[1]}`;
+};
+
+const AliOssUpload = (props) => {
+  // const { type, returnImageUrl }
+};
---
+++ client/web/components/AliOssUpload/secret.js
@@ -0,0 +1,3 @@
+export const accessKeyId = "your access key";
+export const accessKeySecret = "your access secret";
+export const bucket = "your bucket name";
